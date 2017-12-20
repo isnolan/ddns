16a68b554771eecda01562fe3ab7441beacae099
yhostc <yhostc@gmail.com>
2017-12-20 21:52:04 +0800

    Update dnspod.py
    add get ip by local network method

--- dnspod.py
+++ dnspod.py
@@ -36,6 +36,11 @@ def getip():
     conn.close()
     return ip
 
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
 if __name__ == '__main__':
     while True:
         try:
