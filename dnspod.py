20730fce7f655758712151eafe2f345433749b73
yhostc <yhostc@gmail.com>
2022-07-16 17:15:51 +0800

    Update dnspod.py
    support for python3

--- dnspod.py
+++ dnspod.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #-*- coding:utf-8 -*-
 
-import httplib, urllib, ssl
+import http.client, urllib
 import socket
 import time
 
@@ -10,17 +10,16 @@ params = dict(
     format="json",
     domain_id=YOUR_DOMAIN_ID, # replace with your domain_od, can get it by API Domain.List
     record_id=YOUR_RECORD_ID, # replace with your record_id, can get it by API Record.List
-    sub_domain="sj", # replace with your sub_domain
+    sub_domain="osx",
     record_line="默认",
 )
 current_ip = None
 
 def ddns(ip):
     params.update(dict(value=ip))
-    ssl._create_default_https_context = ssl._create_unverified_context
     headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/json"}
-    conn = httplib.HTTPSConnection("dnsapi.cn")
-    conn.request("POST", "/Record.Ddns", urllib.urlencode(params), headers)
+    conn = http.client.HTTPSConnection("dnsapi.cn")
+    conn.request("POST", "/Record.Ddns", urllib.parse.urlencode(params), headers)
     
     response = conn.getresponse()
     #print response.status, response.reason
@@ -30,10 +29,10 @@ def ddns(ip):
     return response.status == 200
 
 def getip():
-    conn = httplib.HTTPConnection("members.3322.org")
+    conn = http.client.HTTPConnection("members.3322.org")
     conn.request('GET', '/dyndns/getip', '', {'user-agent':'ddns for dnspod'})
     ip = conn.getresponse().read()
-    print ip
+    print(ip)
     conn.close()
     return ip
 
@@ -42,15 +41,18 @@ def get_ip_address():
     s.connect(("8.8.8.8", 80))
     return s.getsockname()[0]
 
+
 if __name__ == '__main__':
     while True:
         try:
-            ip = getip()
-            #print ip
+            # ip = getip()
+            ip = get_ip_address()
+            print(ip)
             if current_ip != ip:
                 if ddns(ip):
                     current_ip = ip
-        except Exception, e:
-            print e
+        except Exception as e:
+            print(e)
             pass
         time.sleep(30)
+
